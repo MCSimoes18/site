501571dc5d4ea248ac4e288fe73bfafc55cf8a95
github username <github email address>
2019-05-22 19:49:36 -0400

    setup

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class MyInfo extends React.Component {
+  render () {
+      return (
+        <div>
+          <h2>About Me</h2>
+          <h2>Projects</h2>
+          <h2>Skills</h2>
+          <h2>Contact</h2>
+        </div>
+      )
+    }
+  }
+
+export default MyInfo
